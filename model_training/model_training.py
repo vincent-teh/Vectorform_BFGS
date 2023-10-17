e7f421f75d21d47b5fe1bc977bef453543857843
vincent <yusheng.teh13@gmail.com>
2023-10-17 12:17:40 +0800

    fixed type checking

--- model_training/model_training.py
+++ model_training/model_training.py
@@ -71,8 +71,7 @@ def train_for_n_epochs(model: nn.Module,
         print(f'{optimizer.__class__.__name__}\'s loss == {train_losses[-1]}')
         train_times.append(time.time() - start_time) # In second
         accuracies.append(testing_evaluation(model, testing_dataloader))
-        if isinstance(loss, float) and math.isnan(loss) \
-            or torch.isnan(loss).any():
+        if isinstance(loss, float) and math.isnan(loss) or torch.isnan(loss).any():
             print(f"===Loss value drop to NaN at epoch {iteration}, stop training===")
             break
     return train_losses, accuracies, train_times
