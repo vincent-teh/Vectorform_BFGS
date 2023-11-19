5abeebb917677c8097bf308dbabae10ad1295825
vincent <yusheng.teh13@gmail.com>
2023-11-19 21:20:36 +0800

    fix unused variables

--- conjgrad/ConjGrad.py
+++ conjgrad/ConjGrad.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, get_args, Iterable, Literal, Optional, Union
+from typing import Any, Callable, Dict, get_args, Iterable, Literal, Union
 
 import torch
 from torch.optim import Optimizer
