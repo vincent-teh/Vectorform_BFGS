b417ab540bf78ff0227e8ad2d85d9ebe5312db51
vincent <yusheng.teh13@gmail.com>
2023-12-13 15:15:28 +0800

    clean up unrelated file to repo.

--- two_order_utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .two_order_line_search import _strong_wolfe, LineSearch_n_Update
-from .two_order_operator import _add_grad, _clone_param, _directional_evaluate, gather_flat_grad, gather_flat_param, _numel, _params_t, _set_param
--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .anim import create_animation
-from .Rosenbrock import Rosenbrock, run_optimization
